692dfa8747e26af9fafb02d3b96661d5862a9e74
Bruno Vicenzo <bruno@vicenzo.me>
2020-10-21 20:46:22 -0300

    Gem skeleton

---
+++ lib/bra_documents.rb
@@ -0,0 +1,6 @@
+require "bra_documents/version"
+
+module BraDocuments
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bra_documents/version.rb
@@ -0,0 +1,3 @@
+module BraDocuments
+  VERSION = "0.1.0"
+end
