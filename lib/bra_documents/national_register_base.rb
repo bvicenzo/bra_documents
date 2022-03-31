e1b6c9a38e9433f6f60335163b58f20714ebed76
Bruno Vicenzo <bruno@alumni.usp.br>
2022-03-31 15:18:04 -0300

    Add verification digit checker for CPF generator

--- lib/bra_documents/national_register_base.rb
+++ lib/bra_documents/national_register_base.rb
@@ -3,7 +3,6 @@
 module BraDocuments
   class NationalRegisterBase
     class << self
-      NOT_NUMBER_PATTERN = /\D/.freeze
       BASE = 11
 
       private
@@ -14,7 +13,7 @@ module BraDocuments
       end
 
       def number_for(number_description, number_size, given_value)
-        given_value = only_digits_for(given_value)
+        given_value = Formatter.raw(given_value.to_s)
         if !given_value.to_s.empty?
           raise ArgumentError, "#{number_description} number must be a number with #{number_size} digits." unless given_value.size == number_size
           given_value.split('').map(&:to_i)
@@ -25,10 +24,6 @@ module BraDocuments
 
       def number_with(size)
         size.times.map { rand(10) }
-      end
-
-      def only_digits_for(number)
-        number.to_s.gsub(NOT_NUMBER_PATTERN, '')
       end
 
       def verification_digit_for(numbers)
